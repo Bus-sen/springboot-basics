9520f2a1b061f015093b18a6bec6afef0f16e0f2
Bus-sen <snbusra66@gmail.com>
2025-09-09 18:18:03 +0300

    [SP-7] - mapping

--- src/main/java/com/busra/springbootbasics/service/impl/UserServiceImpl.java
+++ src/main/java/com/busra/springbootbasics/service/impl/UserServiceImpl.java
@@ -7,19 +7,32 @@ import com.busra.springbootbasics.repository.UserRepository;
 import com.busra.springbootbasics.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     @Transactional
     public UserResponse create(UserRequest userRequest) {
+        User user = modelMapper.map(userRequest, User.class);   //request -> entity dönüşümü
+        userRepository.save(user);
+        return new UserResponse("Kullanıcı oluşturuldu");
+        /*
+        UserResponse userResponse = modelMapper.map(user, UserResponse.class);  //entity -> response dönüşümü
+        return userResponse;
+         */
+
+
+        /*
         User user = new User();
         user.setUsername(userRequest.getUsername());
         user.setPassword(userRequest.getPassword());
@@ -28,6 +41,7 @@ public class UserServiceImpl implements UserService {
         UserResponse userResponse = new UserResponse();
         userResponse.setMessage("Kullanıcı oluşturuldu");
         return userResponse;
+         */
 /*
         if (true){
             throw new RuntimeException("Rollback test");
@@ -51,6 +65,15 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UserResponse update(long id, User user) {
+        User updatedUser = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User with id " + id + " does not exist"));
+
+        modelMapper.map(user, updatedUser); //user -> updatedUser dönüşümü
+        updatedUser.setId(id);
+        userRepository.save(updatedUser);
+        return new UserResponse("Kullanıcı güncellendi");
+
+        /*
         if (userRepository.findById(id).isPresent()) {
             User updatedUser = userRepository.findById(id).get();
             updatedUser.setUsername(user.getUsername());
@@ -64,6 +87,7 @@ public class UserServiceImpl implements UserService {
         UserResponse userResponse = new UserResponse();
         userResponse.setMessage("Kullanıcı güncellendi");
         return userResponse;
+         */
     }
 
     @Override
